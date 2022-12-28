9c0162f346a914917b3c557070993c4ef1d40d02
balintcristian <cristi.balint11@gmail.com>
2022-12-28 16:37:20 +0200

    varianta finala .

--- meniu-react/src/components/product/product.jsx
+++ meniu-react/src/components/product/product.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 
 const Product = ({ data }) => {
-  const { name, type, price, description } = data;
+  const { nume, categorie, pret} = data;
 
   return (
     <div className="card col-md-6 col-lg-4 product">
-      <img src={`images/${name}.jpg`} className="card-img-top" alt={description?`${name} ${description}`:`${name}`} 
+      <img src={`images/${nume}.jpg`} className="card-img-top" alt={`${categorie}`} 
  onError={({ currentTarget }) => {
     currentTarget.onerror = null;
     currentTarget.src="https://dummyimage.com/640x640/ffffff/000000.png&text=not+found";
@@ -13,11 +13,11 @@ const Product = ({ data }) => {
   }} />
       <div className="card-body">
         <h5 className="card-title">
-          {description ? `${name} ${description}` : name}
+          {nume}
         </h5>
         <p className="card-text">
-          {price} lei /{" "}
-          {type === "bere" || type === "vin" ? ` sticla` : `50 ml`}
+          {pret} lei /{" "}
+          {categorie === "bere" || categorie === "vin" ? ` sticla` : `50 ml`}
         </p>
         {/*
         <a href="#" className="btn btn-primary">
